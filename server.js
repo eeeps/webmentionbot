2b8920a92888d4fcf461b31441d5174fdd541e4a
Glitch (glitch-hello-node) <none>
2022-01-22 00:19:35 +0000

    🐩🚣 Checkpoint ./server.js:415762/466

--- server.js
+++ server.js
@@ -11,47 +11,44 @@ const fs = require('fs');
 
 // Declare a route
 fastify.get('/', async function( request, reply ) {
-    console.log('hi');
 
-  await fs.readFile('receivedBeacons.json', 'utf8', function(err, data){
-    if (err) throw err;
-    reply.send(data);
-  });
+  await fs.readFile( 'receivedBeacons.json', 'utf8', function( err, data ) {
+    if ( err ) throw err;
+    reply.send( data );
+  } );
   
-});
+} );
 
-fastify.post('/post', async function( request, reply ) {
+fastify.post( '/post', function( request, reply ) {
   
-  const parsedBody = JSON.parse(request.body);
+  const oldLogString = fs.readFileSync( 'receivedBeacons.json', 'utf8' ),
+        oldLog = JSON.parse( oldLogString );
   
-  const oldLogString = fs.readFileSync('receivedBeacons.json', 'utf8');
-  console.log(oldLogString);
-  const oldLog = JSON.parse( oldLogString );
-  
+  const parsedBody = JSON.parse( request.body );
   const newLogItem = {
     time: new Date(),
-    ip: request.ips[request.ips.length - 1],
-    userAgent: request.headers["user-agent"],
+    ip: request.ips[ request.ips.length - 1 ],
+    userAgent: request.headers[ "user-agent" ],
     url: parsedBody.url,
     success: parsedBody.success
   };
   
   const newLog = [ newLogItem ].concat( oldLog ),
-        logString = JSON.stringify(newLog, null, 2);
+        logString = JSON.stringify( newLog, null, 2 );
   
-  await fs.writeFile('receivedBeacons.json', logString, function (err) {
-    if (err) throw err;
-  });
+  fs.writeFile( 'receivedBeacons.json', logString, function( err ) {
+    if ( err ) throw err;
+  } );
   
   reply.send( JSON.stringify( newLogItem, null, 2 ) ); // helped me test...
   
-});
+} );
 
 // Run the server!
-fastify.listen(3000, function (err, address) {
-  if (err) {
-    fastify.log.error(err)
-    process.exit(1)
+fastify.listen( 3000, function ( err, address ) {
+  if ( err ) {
+    fastify.log.error( err );
+    process.exit( 1 );
   }
   // Server is now listening on ${address}
-});
+} );
