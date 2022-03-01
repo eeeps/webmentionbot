1aa718a7c80269b77dd022182d2b5bec2c4f72df
Glitch (glitch-hello-node) <none>
2022-03-01 03:43:38 +0000

    🚶✌️ Checkpoint ./server.js:415762/949 ./public/index.html:415762/55

--- server.js
+++ server.js
@@ -1,17 +1,37 @@
 
-// Require the framework and instantiate it
-
-// CommonJs
+// fastify
 const fastify = require( 'fastify' )( {
   logger: true,
 //  trustProxy: true // needed to get ips...
 } );
 
-const fs = require( 'fs' );
+// server-sent events stuff
 const { FastifySSEPlugin } = require( 'fastify-sse-v2' );
 // const EventIterator = require('event-iterator');
 const { on, EventEmitter } = require( 'events' );
 fastify.register( FastifySSEPlugin) ;
+
+// serve GET root statically
+const path = require('path');
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public')
+})
+
+
+// receive new beacons
+
+fastify.post( '/', function( request, reply ) {
+
+  const newLogItem = request.body;
+  console.log( request.body );
+  ee.emit( 'update', newLogItem );  
+  reply.send( JSON.stringify( newLogItem ) ); // helped me test...
+  
+} );
+
+
+
+// send events to EventSource subscribers (like index.html)
 
 const ee = new EventEmitter();
 
@@ -30,15 +50,6 @@ fastify.get( "/events", function ( req, res ) {
   );
 });
 
-
-fastify.post( '/', function( request, reply ) {
-
-  const newLogItem = request.body;
-  console.log( request.body );
-  ee.emit( 'update', newLogItem );  
-  reply.send( JSON.stringify( newLogItem ) ); // helped me test...
-  
-} );
 
 
 fastify.listen( 3000, function ( err, address ) {
