d896c12139b28594429cbac205c360d2d2da4f8d
Glitch (glitch-hello-node) <none>
2022-04-14 19:52:12 +0000

    👠🛥 Checkpoint ./server.js:415762/682

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ fastify.post( '/', ( req, reply ) => {
   if ( !( req.body.source && req.body.target ) ) {
     reply
       .code( 400 )
-      .send( "POST request must contain x-www-form-urlencoded source and target parameters" )
+      .send( "POST request must contain x-www-form-urlencoded source and target parameters" );
+    return;
   }
   
   // ...the receiver should verify the parameters...
@@ -29,12 +30,24 @@ fastify.post( '/', ( req, reply ) => {
   // 3.2.1 Request Verification
   // The receiver must check that source and target are valid URLs...
   
-  const sourceURL = new URL( req.body.source );
-  const targetURL = new URL( req.body.target );
-  
+  let sourceURL, targetURL;
+  try {
+    sourceURL = new URL( req.body.source );
+    targetURL = new URL( req.body.target );
+  } catch {
+    reply.code( 400 ).send( "source and target must be valid URLs" );
+    return;
+  }
   
   // ...and are of schemes that are supported by the receiver.
   // (Most commonly this means checking that the source and target schemes are http or https).
+  
+  const acceptableProtocols = [ 'http:', 'https:' ]
+  if ( !acceptableProtocols.contains( sourceURL.protocol ) || 
+       !acceptableProtocols.contains( targetURL.protocol ) ) {
+    reply.code( 400 ).send( "Source and target must be HTTP: or HTTPS:" );
+    return;
+  }
   
   // ...and then should queue and process the request asynchronously, to prevent DoS attacks.
   
