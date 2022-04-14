bfa7683edd718c90f09487cb8fa828415c0ad73a
Glitch (glitch-hello-node) <none>
2022-04-14 18:27:49 +0000

    😻🗃 Checkpoint ./server.js:415762/1313

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const fastify = require( 'fastify' )( {
 //  trustProxy: true // needed to get ips...
 } );
 
+const url = require( 'url' );
+
 // handle posts with formbodys
 fastify.register( require( 'fastify-formbody' ) );
 
@@ -12,9 +14,33 @@ fastify.register( require( 'fastify-formbody' ) );
 // receive posts
 
 fastify.post( '/', ( req, reply ) => {
+  
+  // 3.2 Receiving Webmentions
+  // Upon receipt of a POST request containing the source and target parameters...
+  
+  if ( !( req.body.source && req.body.target ) ) {
+    reply
+      .code( 400 )
+      .send( "POST request must contain x-www-form-urlencoded source and target parameters" )
+  }
+  
+  // ...the receiver should verify the parameters...
+  
+  // 3.2.1 Request Verification
+  // The receiver must check that source and target are valid URLs...
+  
+  const myURL = new URL('https://example.org/abc/xyz?123');
+  
+  
+  // ...and are of schemes that are supported by the receiver.
+  // (Most commonly this means checking that the source and target schemes are http or https).
+  
+  // ...and then should queue and process the request asynchronously, to prevent DoS attacks.
+  
   reply
-    .code(202)
-    .send( req.body )
+    .code( 202 )
+    .send( req.body );
+  
 } );
 
 
