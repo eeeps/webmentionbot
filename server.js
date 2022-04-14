760bdd7c2d9fa3c211ba2f4ed94d5e123c18374f
Glitch (glitch-hello-node) <none>
2022-04-14 17:43:02 +0000

    👾🎠 Checkpoint ./server.js:415762/1505 ./package.json:415762/892

--- server.js
+++ server.js
@@ -5,62 +5,15 @@ const fastify = require( 'fastify' )( {
 //  trustProxy: true // needed to get ips...
 } );
 
-// server-sent events stuff
-const { FastifySSEPlugin } = require( 'fastify-sse-v2' );
-// const EventIterator = require('event-iterator');
-const { on, EventEmitter } = require( 'events' );
-fastify.register( FastifySSEPlugin) ;
+// handle posts with formbodys
+fastify.register( require( 'fastify-formbody' ) );
 
-// serve GET root statically
-const path = require('path');
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public')
+
+// receive posts
+
+fastify.post('/', (req, reply) => {
+  reply.send(req.body)
 })
-
-
-// receive new beacons
-
-fastify.post( '/:path', function( request, reply ) {
-
-  const path = request.params.path; // only captures before first slash
-  const body = request.body;
-  const query = request.query;
-  
-  const newLogItem = {
-    path,
-    queryParameters: { ...query },
-    body: JSON.parse( JSON.stringify( body ) )
-  }
-  
-  // console.log( newLogItem );
-  
-  ee.emit( 'update', newLogItem );  
-  reply.send( JSON.stringify( newLogItem ) ); // helped me test...
-  
-} );
-
-
-
-// send events to EventSource subscribers (like index.html)
-
-const ee = new EventEmitter();
-
-fastify.get( "/events", function ( req, res ) {
-  res.header( "Access-Control-Allow-Origin", "*" );
-  res.sse(
-    ( async function* () {
-      for await ( const event of on( ee, 'update' ) ) {
-        for ( const e of event ) {
-          console.log(e);
-          yield {
-            id: (new Date()).toISOString(),
-            data: JSON.stringify( e )
-          };
-        }
-      }
-    } )()
-  );
-});
 
 
 
